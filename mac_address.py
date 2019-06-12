9c47a1e9fbfbf01b1044b66e63c7c7a647435d2d
yugan6669 <45059895+yugan6669@users.noreply.github.com>
2019-06-12 22:15:23 +0530

    changes committed

--- mac_address.py
+++ mac_address.py
@@ -1,8 +1,10 @@
-import urllib.request
+url = 'https://api.macaddress.io/v1?apiKey=at_UPv75gGwN4f9sDt6AYVBC76aBhgTj&output=json&search=44:38:39:ff:ef:57'
+import urllib
+urllib.urlopen(url)
 import json
 import codecs
-url = 'https://api.macaddress.io/v1?apiKey=at_UPv75gGwN4f9sDt6AYVBC76aBhgTj&output=json&search=44:38:39:ff:ef:57'
-json_obj = urllib.request.urlopen(url)
+#url = 'https://api.macaddress.io/v1?apiKey=at_UPv75gGwN4f9sDt6AYVBC76aBhgTj&output=json&search=44:38:39:ff:ef:57'
+json_obj = urllib.urlopen(url)
 reader = codecs.getreader("utf-8")
 data = json.load(reader(json_obj))
 #print (data)
