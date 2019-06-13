804ca97ba06c9d7f8f9b53fe0edd09c0df4701df
yugan6669 <45059895+yugan6669@users.noreply.github.com>
2019-06-13 16:47:00 +0530

    Update docker_exe_script.sh

--- docker_exe_script.sh
+++ docker_exe_script.sh
@@ -1,3 +1,6 @@
 #echo "Execute docker build and docker run commands.
+
+#Build the Image with the help of Dockerfile
 docker build -t mac_address .
-docker run -it mac_address
+#Run the container and pass runtime arguments i.e., API_KEY and MAC_ADDRESS.
+docker run -it mac_address arg1 arg2
