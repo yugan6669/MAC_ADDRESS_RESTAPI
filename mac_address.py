4161afb78010a7d1f7801074f7fad82f4b313b81
yugan6669 <45059895+yugan6669@users.noreply.github.com>
2019-06-13 12:17:18 +0530

    Changes are Committed.

--- mac_address.py
+++ mac_address.py
@@ -1,12 +1,21 @@
-url = 'https://api.macaddress.io/v1?apiKey=at_UPv75gGwN4f9sDt6AYVBC76aBhgTj&output=json&search=44:38:39:ff:ef:57'
-import urllib
-urllib.urlopen(url)
+#!/usr/bin/python3
+
+#writing the program for getting the data from Rest API database with the help of passing runtime arguments.
+
+#importing the default packages
+import urllib.request
 import json
 import codecs
-#url = 'https://api.macaddress.io/v1?apiKey=at_UPv75gGwN4f9sDt6AYVBC76aBhgTj&output=json&search=44:38:39:ff:ef:57'
-json_obj = urllib.urlopen(url)
+#paasing the arguments at runtime using sys
+import sys
+API_KEY=sys.argv[1]
+MAC_ADDRESS=sys.argv[2]
+#Getting the data from restapi url
+url = 'https://api.macaddress.io/v1?apiKey='+API_KEY+'&output=json&search='+MAC_ADDRESS
+#executing the steps to get output
+json_obj = urllib.request.urlopen(url)
 reader = codecs.getreader("utf-8")
 data = json.load(reader(json_obj))
-#print (data)
+#Printing the data from api based database.
 print ("company Name is " +data['vendorDetails']['companyName']);
 print ("comapany Address is " +data['vendorDetails']['companyAddress']);
