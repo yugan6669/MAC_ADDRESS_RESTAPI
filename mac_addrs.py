8e1d908662c6a44c91ca27dc55460ac874afb52e
yugan6669 <45059895+yugan6669@users.noreply.github.com>
2019-06-13 19:32:13 +0530

    Changes are committed

--- mac_addrs.py
+++ mac_addrs.py
@@ -17,5 +17,33 @@ json_obj = urllib.request.urlopen(url)
 reader = codecs.getreader("utf-8")
 data = json.load(reader(json_obj))
 #Printing the data from api based database.
-print ("company Name is " +data['vendorDetails']['companyName']);
-print ("comapany Address is " +data['vendorDetails']['companyAddress']);
+print ("Below details are vendorDetails: ")
+print ("company Name is: " +data['vendorDetails']['companyName']);
+print ("comapany Address is: " +data['vendorDetails']['companyAddress']);
+print ("countryCode is: " +data['vendorDetails']['countryCode']);
+print ("oui is: " +data['vendorDetails']['oui']);
+print ("This is Private Company!:  " +data['vendorDetails']['isPrivate']);
+print ("---------END of VendorDetails------------------");
+#Below Details are blockDetails
+print ("Below details are blockDetails: ")
+print ("blockFound is: " +data['blockDetails']['blockFound']);
+print ("borderLeft is: " +data['blockDetails']['borderLeft']);
+print ("borderRight is: " +data['blockDetails']['borderRight']);
+print ("blockSize is: " +data['blockDetails']['blockSize']);
+print ("assignmentBlockSize is: " +data['blockDetails']['assignmentBlockSize']);
+print ("dateCreated is: " +data['blockDetails']['dateCreated']);
+print ("dateUpdated is: " +data['blockDetails']['dateUpdated']);
+print ("----------END of blockDetails-------------------");
+#Below Details are MacAddressDetails
+print ("Below details are MacAddressDetails: ")
+print ("MacAddress is: " +data['macAddressDetails']['searchTerm']);
+print ("isValid: " +data['macAddressDetails']['isValid']);
+print ("virtualMachine is: " +data['macAddressDetails']['virtualMachine']);
+print ("transmissionType is: " +data['macAddressDetails']['transmissionType']);
+print ("administrationType is: " +data['macAddressDetails']['administrationType']);
+print ("wiresharkNotes is: " +data['macAddressDetails']['wiresharkNotes']);
+print ("comment is: " +data['macAddressDetails']['comment']);
+print ("---------END of macAddressDetails----------------"); 
+
+
+
