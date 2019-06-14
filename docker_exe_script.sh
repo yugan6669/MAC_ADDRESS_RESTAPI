3aa56f44de2a0d983a6fe2e448bca6a4ffab64d0
yugan6669 <45059895+yugan6669@users.noreply.github.com>
2019-06-14 11:40:36 +0530

    Changes Committed.

--- docker_exe_script.sh
+++ docker_exe_script.sh
@@ -1,10 +1,10 @@
 #echo "Execute docker build and docker run commands.
 #Arguments declaration
-API_KEY=at_UPv75gGwN4f9sDt6AYVBC76aBhgTj
-MAC_ADDRESS=44:38:39:ff:ef:57
+#API_KEY=at_UPv75gGwN4f9sDt6AYVBC76aBhgTj
+#MAC_ADDRESS=44:38:39:ff:ef:57
 #Build the Image with the help of Dockerfile
 #docker build -t mac_address .
 docker build -t mac_addrs -f Dockerfile_Second .
 #Run the container and pass runtime arguments i.e., API_KEY and MAC_ADDRESS.
 #docker run -it mac_address arg1 arg2
-docker run -it mac_addrs $API_KEY $MAC_ADDRESS
+docker run -it mac_addrs
